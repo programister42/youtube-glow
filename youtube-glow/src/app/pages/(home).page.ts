b3025cac2675deacf8e7d13c7675ccf99d28fbd7
Taras Tereschenko <tarrrasio@gmail.com>
2024-08-24 19:31:18 +0500

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/EsNTLl0GAd

---
+++ youtube-glow/src/app/pages/(home).page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { AnalogWelcomeComponent } from './analog-welcome.component';
+
+@Component({
+  selector: 'youtube-glow-home',
+  standalone: true,
+  imports: [AnalogWelcomeComponent],
+  template: `
+     <youtube-glow-analog-welcome/>
+  `,
+})
+export default class HomeComponent {
+}
